5f569aae23f46a8066b0ee929798e78171e0d27b
jumpingrock <kennethyeongky@gmail.com>
2019-02-14 01:09:16 +0800

    mvp-ish

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var monkey = "hello";
-ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
-    document.getElementById('root')
-);
+
+
+var saySomething = {
+    message: function(){
+        console.log("hello!")
+    },
+    speak: function (){
+        for (var i=0; i<tweets.length;i++){
+            console.log("Name:", tweets[i].user.screen_name);
+            console.log("Message:", tweets[i].text);
+            console.log("Source:", tweets[i].source);
+        }
+
+    }
+};
+
+document.querySelector('#root').addEventListener('click',saySomething.message);
+document.querySelector('#replies').addEventListener('click',saySomething.speak);
+document.querySelector('#media').addEventListener('click',saySomething.message);
+document.querySelector('#likes').addEventListener('click',saySomething.speak);
